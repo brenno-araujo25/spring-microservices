eea9b4f700a335261a66f253e44f0935f92c9a83
Brenno Araújo <brennocaldeira@outlook.com>
2025-03-24 12:38:15 -0300

    refactor: refactor UserDTO to use class structure with Lombok annotations

--- user/src/main/java/com/microservices/user/dtos/UserDTO.java
+++ user/src/main/java/com/microservices/user/dtos/UserDTO.java
@@ -2,10 +2,18 @@ package com.microservices.user.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-public record UserDTO(
-    @NotBlank String name,
-    @NotBlank @Email String email
-) {
+@Data
+@AllArgsConstructor
+public class UserDTO {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
 
 }
