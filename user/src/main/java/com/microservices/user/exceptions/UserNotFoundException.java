34fe2e5ddd559ffa97c74d048140ec2d063dbb10
Brenno Araújo <brennocaldeira@outlook.com>
2025-03-24 11:07:39 -0300

    feat: add custom exceptions

---
+++ user/src/main/java/com/microservices/user/exceptions/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.microservices.user.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
