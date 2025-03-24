6abb49c1d039eea104f95d031ffad7c27d1956b7
Brenno Araújo <brennocaldeira@outlook.com>
2025-03-24 11:06:59 -0300

    feat: update UserRepository to include email-based search methods

--- user/src/main/java/com/microservices/user/repositories/UserRepository.java
+++ user/src/main/java/com/microservices/user/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.microservices.user.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.microservices.user.models.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findById(UUID id);
+    boolean existsByEmail(String email);
 }
