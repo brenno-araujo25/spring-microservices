a298eca890559ab612ed98a9233ebe9c96447c14
Brenno Araújo <brennocaldeira@outlook.com>
2025-03-24 11:09:31 -0300

    feat: add user retrieval methods and email existence check

--- user/src/main/java/com/microservices/user/controllers/UserController.java
+++ user/src/main/java/com/microservices/user/controllers/UserController.java
@@ -1,10 +1,14 @@
 package com.microservices.user.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservices.user.dtos.UserDTO;
@@ -14,12 +18,13 @@ import com.microservices.user.services.UserService;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("/users")
+    @PostMapping
     public ResponseEntity<User> saveUser(
         @RequestBody @Valid UserDTO userDTO
     ) {
@@ -27,6 +32,32 @@ public class UserController {
         BeanUtils.copyProperties(userDTO, user);
 
         return ResponseEntity.created(null).body(userService.save(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<User>> findAll() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/email")
+    public ResponseEntity<User> findByEmail(
+        @RequestBody @Valid String email
+    ) {
+        return ResponseEntity.ok(userService.findByEmail(email));
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<User> findById(
+        @RequestBody @Valid UUID id
+    ) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @GetMapping("/exists")
+    public ResponseEntity<Boolean> existsByEmail(
+        @RequestBody @Valid String email
+    ) {
+        return ResponseEntity.ok(userService.existsByEmail(email));
     }
 
 }
--- user/src/main/java/com/microservices/user/services/UserService.java
+++ user/src/main/java/com/microservices/user/services/UserService.java
@@ -1,8 +1,13 @@
 package com.microservices.user.services;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.microservices.user.exceptions.UserAlreadyExistsException;
+import com.microservices.user.exceptions.UserNotFoundException;
 import com.microservices.user.models.User;
 import com.microservices.user.producers.UserProducer;
 import com.microservices.user.repositories.UserRepository;
@@ -20,9 +25,30 @@ public class UserService {
 
     @Transactional
     public User save(User user) {
+        if (userRepository.existsByEmail(user.getEmail()))
+            throw new UserAlreadyExistsException("User already exists with email: " + user.getEmail());
+
         user = userRepository.save(user);
         userProducer.publishMessageEmail(user);
         return user;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("User not found with email: " + email));
+    }
+
+    public User findById(UUID id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
+    }
+
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
     }
 
 }
